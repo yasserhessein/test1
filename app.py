b992a468c8e407726ebb8ec36a1313d983ecf99d
Yasir Hussein Shakir <69808132+yasserhessein@users.noreply.github.com>
2021-07-24 01:28:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,99 +1,174 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import base64
-from PIL import Image
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score, balanced_accuracy_score, precision_score, recall_score, matthews_corrcoef, f1_score, cohen_kappa_score
+import plotly.graph_objects as go
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import load_diabetes
 
-# Calculates performance metrics
-def calc_metrics(input_data):
-    Y_actual = input_data.iloc[:,0]
-    Y_predicted = input_data.iloc[:,1]
-    acc = accuracy_score(Y_actual, Y_predicted)
-    balanced_accuracy = balanced_accuracy_score(Y_actual, Y_predicted)
-    precision = precision_score(Y_actual, Y_predicted, average='weighted')
-    recall = recall_score(Y_actual, Y_predicted, average='weighted')
-    mcc = matthews_corrcoef(Y_actual, Y_predicted)
-    f1 = f1_score(Y_actual, Y_predicted, average='weighted')
-    cohen_kappa = cohen_kappa_score(Y_actual, Y_predicted)
+#---------------------------------#
+# Page layout
+## Page expands to full width
+st.set_page_config(page_title='The Machine Learning Hyperparameter Optimization App',
+    layout='wide')
 
-    acc_series = pd.Series(acc, name='Accuracy')
-    balanced_accuracy_series = pd.Series(balanced_accuracy, name='Balanced_Accuracy')
-    precision_series = pd.Series(precision, name='Precision')
-    recall_series = pd.Series(recall, name='Recall')
-    mcc_series = pd.Series(mcc, name='MCC')
-    f1_series = pd.Series(f1, name='F1')
-    cohen_kappa_series = pd.Series(cohen_kappa, name='Cohen_Kappa')
+#---------------------------------#
+st.write("""
+# The Machine Learning Hyperparameter Optimization App
+**(Regression Edition)**
 
-    df = pd.concat( [acc_series, balanced_accuracy_series, precision_series,
-                     recall_series, mcc_series, f1_series, cohen_kappa_series], axis=1 )
-    return df
+In this implementation, the *RandomForestRegressor()* function is used in this app for build a regression model using the **Random Forest** algorithm.
 
-# Calculates confusion matrix
-def calc_confusion_matrix(input_data):
-    Y_actual = input_data.iloc[:,0]
-    Y_predicted = input_data.iloc[:,1]
-    confusion_matrix_array = confusion_matrix(Y_actual, Y_predicted)
-    confusion_matrix_df = pd.DataFrame(confusion_matrix_array, columns=['Actual','Predicted'], index=['Actual','Predicted'])
-    return confusion_matrix_df
+""")
 
-# Load example data
-def load_example_data():
-    df = pd.read_csv('Y_example.csv')
-    return df
+#---------------------------------#
+# Sidebar - Collects user input features into dataframe
+st.sidebar.header('Upload your CSV data')
+uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
+""")
 
-# Download performance metrics
+# Sidebar - Specify parameter settings
+st.sidebar.header('Set Parameters')
+split_size = st.sidebar.slider('Data split ratio (% for Training Set)', 10, 90, 80, 5)
+
+st.sidebar.subheader('Learning Parameters')
+parameter_n_estimators = st.sidebar.slider('Number of estimators (n_estimators)', 0, 500, (10,50), 50)
+parameter_n_estimators_step = st.sidebar.number_input('Step size for n_estimators', 10)
+st.sidebar.write('---')
+parameter_max_features = st.sidebar.slider('Max features (max_features)', 1, 50, (1,3), 1)
+st.sidebar.number_input('Step size for max_features', 1)
+st.sidebar.write('---')
+parameter_min_samples_split = st.sidebar.slider('Minimum number of samples required to split an internal node (min_samples_split)', 1, 10, 2, 1)
+parameter_min_samples_leaf = st.sidebar.slider('Minimum number of samples required to be at a leaf node (min_samples_leaf)', 1, 10, 2, 1)
+
+st.sidebar.subheader('General Parameters')
+parameter_random_state = st.sidebar.slider('Seed number (random_state)', 0, 1000, 42, 1)
+parameter_criterion = st.sidebar.select_slider('Performance measure (criterion)', options=['mse', 'mae'])
+parameter_bootstrap = st.sidebar.select_slider('Bootstrap samples when building trees (bootstrap)', options=[True, False])
+parameter_oob_score = st.sidebar.select_slider('Whether to use out-of-bag samples to estimate the R^2 on unseen data (oob_score)', options=[False, True])
+parameter_n_jobs = st.sidebar.select_slider('Number of jobs to run in parallel (n_jobs)', options=[1, -1])
+
+
+n_estimators_range = np.arange(parameter_n_estimators[0], parameter_n_estimators[1]+parameter_n_estimators_step, parameter_n_estimators_step)
+max_features_range = np.arange(parameter_max_features[0], parameter_max_features[1]+1, 1)
+param_grid = dict(max_features=max_features_range, n_estimators=n_estimators_range)
+
+#---------------------------------#
+# Main panel
+
+# Displays the dataset
+st.subheader('Dataset')
+
+
+
+#---------------------------------#
+# Model building
+
 def filedownload(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="performance_metrics.csv">Download CSV File</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="model_performance.csv">Download CSV File</a>'
     return href
 
-# Sidebar - Header
-st.sidebar.header('Input panel')
-st.sidebar.markdown("""
-[Example CSV file](https://raw.githubusercontent.com/dataprofessor/model_performance_app/main/Y_example.csv)
-""")
+def build_model(df):
+    X = df.iloc[:,:-1] # Using all column except for the last column as X
+    Y = df.iloc[:,-1] # Selecting the last column as Y
 
-# Sidebar panel - Upload input file
-uploaded_file = st.sidebar.file_uploader('Upload your input CSV file', type=['csv'])
+    st.markdown('A model is being built to predict the following **Y** variable:')
+    st.info(Y.name)
 
-# Sidebar panel - Performance metrics
-performance_metrics = ['Accuracy', 'Balanced_Accuracy', 'Precision', 'Recall','MCC', 'F1', 'Cohen_Kappa']
-selected_metrics = st.sidebar.multiselect('Performance metrics', performance_metrics, performance_metrics)
+    # Data splitting
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=split_size)
+    #X_train.shape, Y_train.shape
+    #X_test.shape, Y_test.shape
 
-# Main panel
-image = Image.open('logo.png')
-st.image(image, width = 500)
-st.title('Model Performance Calculator App')
-st.markdown("""
-This app calculates the model performance metrics given the actual and predicted values.
-* **Python libraries:** `base64`, `pandas`, `streamlit`, `scikit-learn`
-""")
+    rf = RandomForestRegressor(n_estimators=parameter_n_estimators,
+        random_state=parameter_random_state,
+        max_features=parameter_max_features,
+        criterion=parameter_criterion,
+        min_samples_split=parameter_min_samples_split,
+        min_samples_leaf=parameter_min_samples_leaf,
+        bootstrap=parameter_bootstrap,
+        oob_score=parameter_oob_score,
+        n_jobs=parameter_n_jobs)
 
+    grid = GridSearchCV(estimator=rf, param_grid=param_grid, cv=5)
+    grid.fit(X_train, Y_train)
+
+    st.subheader('Model Performance')
+
+    Y_pred_test = grid.predict(X_test)
+    st.write('Coefficient of determination ($R^2$):')
+    st.info( r2_score(Y_test, Y_pred_test) )
+
+    st.write('Error (MSE or MAE):')
+    st.info( mean_squared_error(Y_test, Y_pred_test) )
+
+    st.write("The best parameters are %s with a score of %0.2f"
+      % (grid.best_params_, grid.best_score_))
+
+    st.subheader('Model Parameters')
+    st.write(grid.get_params())
+
+    #-----Process grid data-----#
+    grid_results = pd.concat([pd.DataFrame(grid.cv_results_["params"]),pd.DataFrame(grid.cv_results_["mean_test_score"], columns=["R2"])],axis=1)
+    # Segment data into groups based on the 2 hyperparameters
+    grid_contour = grid_results.groupby(['max_features','n_estimators']).mean()
+    # Pivoting the data
+    grid_reset = grid_contour.reset_index()
+    grid_reset.columns = ['max_features', 'n_estimators', 'R2']
+    grid_pivot = grid_reset.pivot('max_features', 'n_estimators')
+    x = grid_pivot.columns.levels[1].values
+    y = grid_pivot.index.values
+    z = grid_pivot.values
+
+    #-----Plot-----#
+    layout = go.Layout(
+            xaxis=go.layout.XAxis(
+              title=go.layout.xaxis.Title(
+              text='n_estimators')
+             ),
+             yaxis=go.layout.YAxis(
+              title=go.layout.yaxis.Title(
+              text='max_features')
+            ) )
+    fig = go.Figure(data= [go.Surface(z=z, y=y, x=x)], layout=layout )
+    fig.update_layout(title='Hyperparameter tuning',
+                      scene = dict(
+                        xaxis_title='n_estimators',
+                        yaxis_title='max_features',
+                        zaxis_title='R2'),
+                      autosize=False,
+                      width=800, height=800,
+                      margin=dict(l=65, r=50, b=65, t=90))
+    st.plotly_chart(fig)
+
+    #-----Save grid data-----#
+    x = pd.DataFrame(x)
+    y = pd.DataFrame(y)
+    z = pd.DataFrame(z)
+    df = pd.concat([x,y,z], axis=1)
+    st.markdown(filedownload(grid_results), unsafe_allow_html=True)
+
+#---------------------------------#
 if uploaded_file is not None:
-    input_df = pd.read_csv(uploaded_file)
-    confusion_matrix_df = calc_confusion_matrix(input_df)
-    metrics_df = calc_metrics(input_df)
-    selected_metrics_df = metrics_df[ selected_metrics ]
-    st.header('Input data')
-    st.write(input_df)
-    st.header('Confusion matrix')
-    st.write(confusion_matrix_df)
-    st.header('Performance metrics')
-    st.write(selected_metrics_df)
-    st.markdown(filedownload(selected_metrics_df), unsafe_allow_html=True)
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
+    build_model(df)
 else:
-    st.info('Awaiting the upload of the input file.')
-    if st.button('Use Example Data'):
-        input_df = load_example_data()
-        confusion_matrix_df = calc_confusion_matrix(input_df)
-        metrics_df = calc_metrics(input_df)
-        selected_metrics_df = metrics_df[ selected_metrics ]
-        st.header('Input data')
-        st.write(input_df)
-        st.header('Confusion matrix')
-        st.write(confusion_matrix_df)
-        st.header('Performance metrics')
-        st.write(selected_metrics_df)
-        st.markdown(filedownload(selected_metrics_df), unsafe_allow_html=True)
+    st.info('Awaiting for CSV file to be uploaded.')
+    if st.button('Press to use Example Dataset'):
+        diabetes = load_diabetes()
+        X = pd.DataFrame(diabetes.data, columns=diabetes.feature_names)
+        Y = pd.Series(diabetes.target, name='response')
+        df = pd.concat( [X,Y], axis=1 )
+
+        st.markdown('The **Diabetes** dataset is used as the example.')
+        st.write(df.head(5))
+
+        build_model(df)
